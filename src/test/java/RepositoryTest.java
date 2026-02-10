012ded20b814edcd4261a9f4217d853eb1854fbc
Zaiats Andrii <andriy.s.zayats@gmail.com>
2026-02-10 14:47:17 +0200

    minor test fix

--- src/test/java/RepositoryTest.java
+++ src/test/java/RepositoryTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import university.domain.Faculty;
 import university.domain.Teacher;
@@ -16,25 +17,14 @@ public class RepositoryTest {
     private static Teacher teacher;
     public static Faculty faculty1;
     public static Faculty faculty2;
-    public static FacultyRepository repo;
+    public FacultyRepository repo;
 
-    @BeforeAll
-    public static void setupTest(){
+    @BeforeEach
+    public void setupTest(){
         teacher = new Teacher("TestTeacherID", "","","", LocalDate.now(), "","","","","",LocalDate.now(),1);
         faculty1 = new Faculty("1", "", "", teacher, "");
         faculty2 = new Faculty("2", "", "", teacher, "");
         repo = new FacultyRepository();
-    }
-    @Test
-    public void testFindAll(){
-        repo.add(faculty1);
-        repo.add(faculty2);
-
-        assertEquals(faculty1, repo.findAll().get(0), "should be equal");
-        assertEquals(faculty2, repo.findAll().get(1), "should be equal");
-
-        assertNotEquals(faculty1, repo.findAll().get(1), "shouldn't be equal");
-        assertNotEquals(faculty2, repo.findAll().get(0), "shouldn't be equal");
     }
     @Test
     public void testFindByID(){
@@ -55,4 +45,16 @@ public class RepositoryTest {
                 "IllegalArgumentException was expected" // Optional failure message
         );
     }
+    @Test
+    public void testFindAll(){
+        repo.add(faculty1);
+        repo.add(faculty2);
+
+        assertEquals(faculty1, repo.findAll().get(0), "should be equal");
+        assertEquals(faculty2, repo.findAll().get(1), "should be equal");
+
+        assertNotEquals(faculty1, repo.findAll().get(1), "shouldn't be equal");
+        assertNotEquals(faculty2, repo.findAll().get(0), "shouldn't be equal");
+    }
+
 }
