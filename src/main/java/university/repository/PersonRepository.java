4f641858f43c8edfcbc0c925a1a7d224ec397a4e
Zaiats Andrii <andriy.s.zayats@gmail.com>
2026-03-04 23:20:55 +0200

    feat: add get method to PersonRepository

--- src/main/java/university/repository/PersonRepository.java
+++ src/main/java/university/repository/PersonRepository.java
@@ -3,4 +3,12 @@ package university.repository;
 import university.domain.Person;
 
 public class PersonRepository extends Repository<Person, String>{
+    private static PersonRepository personRepository;
+
+    public static PersonRepository get(){
+        if (personRepository == null){
+            personRepository = new PersonRepository();
+        }
+        return personRepository;
+    }
 }
