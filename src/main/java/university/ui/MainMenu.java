2c9a23c89a1e0d5c2b045f0e95e52176b9f28c0f
effloresco <1@mail.com>
2026-03-05 22:45:38 +0200

    refactor

--- src/main/java/university/ui/MainMenu.java
+++ src/main/java/university/ui/MainMenu.java
@@ -1,16 +1,14 @@
 package university.ui;
-
 import static university.service.ReportService.*;
 import static university.service.SearchService.*;
-
 import java.util.Scanner;
 
-public class ConsoleInterface {
+public class MainMenu {
     private final Scanner scanner = new Scanner(System.in);
     private final ManagementMenu management = new ManagementMenu();
 
     public static void main(String[] args) {
-        ConsoleInterface ui = new ConsoleInterface();
+        MainMenu ui = new MainMenu();
         ui.run();
     }
 
@@ -21,28 +19,32 @@ public class ConsoleInterface {
             System.out.println("2 - Пошук");
             System.out.println("3 - Звіти");
             System.out.println("0 - Вихід з програми");
-            String choice = scanner.nextLine();
-            switch (choice) {
-                case "1":
-                    management.management();
-                    break;
-                case "2":
-                    search();
-                    break;
-                case "3":
-                    reports();
-                    break;
-                case "0":
-                    System.exit(0);
-                default:
-                    System.out.println("Введіть коректне значення");
+            String inputLine = scanner.nextLine();
+            try {
+                int input = Integer.parseInt(inputLine);
+                switch (input) {
+                    case 1:
+                        management.management();
+                        break;
+                    case 2:
+                        search();
+                        break;
+                    case 3:
+                        reports();
+                        break;
+                    case 0:
+                        System.exit(0);
+                    default:
+                        System.out.println("Введіть коректне значення");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Введіть коректне значення");
             }
         }
     }
 
     private void search() {
-        boolean status = true;
-        while (status) {
+        while (true) {
             System.out.println("\n*-Пошук студентів-*");
             System.out.println("1. Пошук за ПІБ");
             System.out.println("2. Пошук за курсом");
@@ -56,15 +58,14 @@ public class ConsoleInterface {
                 case "1" -> searchStudentByFullName();
                 case "2" -> searchByCourse();
                 case "3" -> searchByGroup();
-                case "0" -> status = false;
+                case "0" -> System.exit(0);
                 default -> System.out.println("Невірний вибір!");
             }
         }
     }
 
     private void reports() {
-        boolean status = true;
-        while (status) {
+        while (true) {
             System.out.println("\n*-Списки студентів-*");
             System.out.println("1. За алфавітом");
             System.out.println("2. За курсом");
@@ -76,7 +77,7 @@ public class ConsoleInterface {
             switch (choice) {
                 case "1" -> printAllStudentsAlphabetically();
                 case "2" -> printAllStudentsByCourse();
-                case "0" -> status = false;
+                case "0" -> System.exit(0);
                 default -> System.out.println("Невірний вибір!");
             }
         }
