c41e88249f612a928104056c2e3f872db06a5743
Zaiats Andrii <andriy.s.zayats@gmail.com>
2026-04-20 22:49:39 +0300

    refactor: remove unused storage managers and user initialization logic from MainMenu

--- src/main/java/university/ui/MainMenu.java
+++ src/main/java/university/ui/MainMenu.java
@@ -15,29 +15,6 @@ public class MainMenu {
     private final AuthMenu auth = new AuthMenu();
     private final ReportsMenu reportsMenu = new ReportsMenu();
     private final SearchMenu searchMenu = new SearchMenu();
-    private static final UserRepository userRepository = UserRepository.get(UserRepository.class);
-    private static final UserService userService = new UserService(userRepository);
-    private static final StudentStorageManager studentStorageManager = new StudentStorageManager();
-    private static final TeacherStorageManager teacherStorageManager = new TeacherStorageManager();
-    private static final DepartmentStorageManager departmentStorageManager = new DepartmentStorageManager();
-    private static final FacultyStorageManager facultyStorageManager = new FacultyStorageManager();
-    private static final ServiceStorageManager serviceStorageManager = new ServiceStorageManager();
-    private static final UserStorageManager userStorageManager = new UserStorageManager();
-
-    static {
-        //loads all data from the storage
-        studentStorageManager.loadAllData();
-        teacherStorageManager.loadAllData();
-        departmentStorageManager.loadAllData();
-        facultyStorageManager.loadAllData();
-        serviceStorageManager.loadAllData();
-        userStorageManager.loadAllData();
-        //adds default admin user
-        try {
-            userService.createUser(new User("admin", "12345678", User.PERMISSION_VIEW | User.PERMISSION_EDIT | User.PERMISSION_MANAGE_USERS));
-        } catch(UserAlreadyExistsException | UsernameAlreadyUsedException _){}
-    }
-
 
 public static void main() {
         MainMenu ui = new MainMenu();
